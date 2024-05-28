8b703b4d6e1457b1035d031b12cae54fb1d3ca6b
Huw Jones <huw@pexip.com>
2024-05-28 14:43:58 +0100

    Prepare Pexip packaging.

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/python-ldaptor/issues
+# Bug-Submit: https://github.com/<user>/python-ldaptor/issues/new
+# Changelog: https://github.com/<user>/python-ldaptor/blob/master/CHANGES
+# Documentation: https://github.com/<user>/python-ldaptor/wiki
+# Repository-Browse: https://github.com/<user>/python-ldaptor
+# Repository: https://github.com/<user>/python-ldaptor.git
